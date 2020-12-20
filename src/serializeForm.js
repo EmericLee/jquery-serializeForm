4a53f7dda8c4bced64ec6e12a0f815a47e649710
EmericLee <kometo@gmail.com>
2020-12-20 15:11:28 +0800

    Use serializeArray()

--- src/serializeForm.js
+++ src/serializeForm.js
@@ -9,59 +9,22 @@
  * Licensed under the MIT, GPL licenses.
  */
 (function( $ ){
+
 	$.fn.serializeForm = function() {
-  
-	  // don't do anything if we didn't get any elements
-	  if ( this.length < 1) { 
-		return false; 
-	  }
-  
-	  var data = {};
-	  var lookup = data; //current reference of data
-	  var selector = ':input[type!="checkbox"][type!="radio"], input:checked';
-	  var parse = function() {
-  
-		// Ignore disabled elements
-		if (this.disabled) {
-		  return;
-		}
-  
-		// data[a][b] becomes [ data, a, b ]
-		var named = this.name.replace(/\[([^\]]+)?\]/g, ',$1').split(',');
-		var cap = named.length - 1;
-		var $el = $( this );
-  
-		// Ensure that only elements with valid `name` properties will be serialized
-		if ( named[ 0 ] ) {
-		  for ( var i = 0; i < cap; i++ ) {
-			// move down the tree - create objects or array if necessary
-			lookup = lookup[ named[i] ] = lookup[ named[i] ] ||
-			  ( (named[ i + 1 ] === "" || named[ i + 1 ] === '0') ? [] : {} );
-		  }
-
-		  // at the end, push or assign the value
-		  if ( lookup.length !==  undefined ) {
-			if( $.isArray( $el.val()) ){
-				lookup.concat( $el.val() );
-			}else{
-				lookup.push( $el.val() );
+		var _ = {};
+		$.map(this.serializeArray(), function(n) {
+		  const keys = n.name.match(/[a-zA-Z0-9_]+|(?=\[\])/g);
+		  if (keys.length > 1) {
+			let tmp = _;
+			pop = keys.pop();
+			for (let i = 0; i < keys.length, j = keys[i]; i++) {
+			  tmp[j] = (!tmp[j] ? (pop == '') ? [] : {} : tmp[j]), tmp = tmp[j];
 			}
-		  }else {
-			lookup[ named[ cap ] ]  = $el.val();
-		  }
+			if (pop == '') tmp = (!Array.isArray(tmp) ? [] : tmp), tmp.push(n.value);
+			else tmp[pop] = n.value;
+		  } else _[keys.pop()] = n.value;
+		});
+		return _;
+	}
 
-		  // assign the reference back to root
-		  lookup = data;
-		}
-	  };
-  
-	  // first, check for elements passed into this function
-	  this.filter( selector ).each( parse );
-  
-	  // then parse possible child elements
-	  this.find( selector ).each( parse );
-  
-	  // return data
-	  return data;
-	};
   }( jQuery ));
